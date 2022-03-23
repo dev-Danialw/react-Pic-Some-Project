4bf0ec7154b5c53acb51eb6ca629eb85ac99fb69
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-23 11:39:11 +0500

    calc total cost ✔

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,6 +4,12 @@ import CartItem from "../components/CartItem";
 
 function Cart() {
   const { cartItems } = useContext(Context);
+
+  const totalCost = 5.99 * cartItems.length;
+  const totalCostDisplay = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
   const cartItemElement = cartItems.map((item) => {
     return <CartItem key={item.id} item={item} />;
   });
@@ -11,7 +17,7 @@ function Cart() {
     <main className="cart-page">
       <h1>Check out</h1>
       {cartItemElement}
-      <p>Total: </p>
+      <p>Total: {totalCostDisplay}</p>
       <div className="order-button">
         <button>Place Order</button>
       </div>
