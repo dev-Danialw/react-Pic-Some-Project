20f0298f90b7101040a8cd0761624b72062ed667
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-23 11:32:36 +0500

    removefrom cart ✔

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../Context";
 
 function CartItem({ item }) {
+  const { removeFromCart } = useContext(Context);
+
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line"></i>
+      <i
+        className="ri-delete-bin-line"
+        onClick={() => removeFromCart(item.id)}
+      ></i>
       <img src={item.url} alt={item.id} width="130px" height="130px" />
       <p>$5.99</p>
     </div>
